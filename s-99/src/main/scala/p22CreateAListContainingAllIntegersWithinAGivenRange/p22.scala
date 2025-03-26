48b5b2a680cf664462c04734bce532561c3bcb60
Vitor Machado <vitorytre@gmail.com>
2025-03-26 05:06:35 -0700

    s99

---
+++ s-99/src/main/scala/p22CreateAListContainingAllIntegersWithinAGivenRange/p22.scala
@@ -0,0 +1,15 @@
+package p22CreateAListContainingAllIntegersWithinAGivenRange
+
+//scala> range(4, 9)
+//res0: List[Int] = List(4, 5, 6, 7, 8, 9)
+
+
+@main
+def main(): Unit = {
+  println(s"input: 4, 9")
+  println(s"output: ${range(4, 9)}")
+}
+
+def range(start: Int, end: Int): List[Int] = {
+  List.range(start, end + 1)
+}
