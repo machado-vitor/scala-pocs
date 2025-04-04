47df914d6b8597ece7405be11e1f8a2d6bf2c4d0
Vitor Machado <vitorytre@gmail.com>
2025-04-04 13:53:43 -0700

    s99

--- s-99/src/main/scala/p28SortingAListOfListsAccordingToLengthOfSublists/p28.scala
+++ s-99/src/main/scala/p28SortingAListOfListsAccordingToLengthOfSublists/p28.scala
@@ -2,14 +2,32 @@ package p28SortingAListOfListsAccordingToLengthOfSublists
 //scala> lsort(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o)))
 //res0: List[List[Symbol]] = List(List('o), List('d, 'e), List('d, 'e), List('m, 'n), List('a, 'b, 'c), List('f, 'g, 'h), List('i, 'j, 'k, 'l))
 
+//scala> lsortFreq(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o)))
+//res1: List[List[Symbol]] = List(List('i, 'j, 'k, 'l), List('o), List('a, 'b, 'c), List('f, 'g, 'h), List('d, 'e), List('d, 'e), List('m, 'n))
 
 @main
 def main(): Unit = {
-  println("input: List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))")
-  println(s"output: ${lsort(List(List('a', 'b', 'c'), List('d', 'e'), List('f', 'g', 'h'), List('d', 'e'), List('i', 'j', 'k', 'l'), List('m', 'n'), List('o')))}")
+  val input = List(
+    List('a', 'b', 'c'),
+    List('d', 'e'),
+    List('f', 'g', 'h'),
+    List('d', 'e'),
+    List('i', 'j', 'k', 'l'),
+    List('m', 'n'),
+    List('o')
+  )
+
+  println("input: " + input)
+  println(s"lsort output: ${lsort(input)}")
+  println(s"lsortFreq output: ${lsortFreq(input)}")
 }
 
 def lsort[A](list: List[List[A]]): List[List[A]] = {
   list.sortBy(_.length)
+}
+
+def lsortFreq[A](list: List[List[A]]): List[List[A]] = {
+  val lengthFreq: Map[Int, Int] = list.groupBy(_.length).view.mapValues(_.size).toMap
+  list.sortBy(sublist => lengthFreq(sublist.length))
 }
 
