b9c775d7f8157ed64daff7ea5a91f11e8cff385e
Vitor Machado <vitorytre@gmail.com>
2025-04-11 16:19:45 -0700

    s99

--- s-99/src/main/scala/arithmetic/p31DetermineWhetherAGivenIntegerNumberIsPrime/S99Int.scala
+++ s-99/src/main/scala/arithmetic/p31DetermineWhetherAGivenIntegerNumberIsPrime/S99Int.scala
@@ -1,8 +1,8 @@
 package arithmetic.p31DetermineWhetherAGivenIntegerNumberIsPrime
 
-import arithmetic.p31DetermineWhetherAGivenIntegerNumberIsPrime.S99Int.primes
-import arithmetic.p31DetermineWhetherAGivenIntegerNumberIsPrime.S99Int.intToS99Int
+import arithmetic.p31DetermineWhetherAGivenIntegerNumberIsPrime.S99Int.{gcd, intToS99Int, primes}
 
+import scala.annotation.tailrec
 import scala.language.implicitConversions
 
 class S99Int(val start: Int) {
@@ -16,10 +16,14 @@ class S99Int(val start: Int) {
 object S99Int {
   val primes: Seq[Int] = LazyList.cons(2, LazyList.from(3, 2) filter { _.isPrime })
 
+  @tailrec
+  def gcd(m: Int, n: Int): Int = if (n == 0) m else gcd(n, m % n)
+
   implicit def intToS99Int(n: Int): S99Int = new S99Int(n)
 }
 
 @main
 def main(): Unit = {
   println(7.isPrime)
+  print(gcd(36, 63))
 }
