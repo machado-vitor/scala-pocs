fa2acd9b8799ee568b9c02289be373acb7f9ce9c
Vitor Machado <vitorytre@gmail.com>
2025-04-14 15:53:53 -0700

    s99

--- s-99/src/main/scala/arithmetic/p31DetermineWhetherAGivenIntegerNumberIsPrime/S99Int.scala
+++ s-99/src/main/scala/arithmetic/p31DetermineWhetherAGivenIntegerNumberIsPrime/S99Int.scala
@@ -1,16 +1,21 @@
 package arithmetic.p31DetermineWhetherAGivenIntegerNumberIsPrime
 
-import arithmetic.p31DetermineWhetherAGivenIntegerNumberIsPrime.S99Int.{gcd, intToS99Int, primes}
-
 import scala.annotation.tailrec
 import scala.language.implicitConversions
 
 class S99Int(val start: Int) {
+  import S99Int._
   //P31
   def isPrime: Boolean =
     (start > 1) && primes.takeWhile {
       _ <= Math.sqrt(start)
     }.forall{ start % _ != 0 }
+
+  //P33
+  def isCoprimeTo(n: Int): Boolean = gcd(start, n) == 1
+
+  //P34
+  def totient: Int = (1 to start).count(start.isCoprimeTo(_))
 }
 
 object S99Int {
@@ -19,11 +24,15 @@ object S99Int {
   @tailrec
   def gcd(m: Int, n: Int): Int = if (n == 0) m else gcd(n, m % n)
 
+
   implicit def intToS99Int(n: Int): S99Int = new S99Int(n)
 }
 
 @main
 def main(): Unit = {
-  println(7.isPrime)
-  print(gcd(36, 63))
+  import S99Int._
+  println(s"7 is prime: ${7.isPrime}")
+  println(s"Greatest common divisor between 36 and 63: ${gcd(36, 63)}")
+  println(s"Determining whether 35 and 64 are coprime: ${35.isCoprimeTo(64)}")
+  println(s"Totient of 10: ${10.totient}")
 }
