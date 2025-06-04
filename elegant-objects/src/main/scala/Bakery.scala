316ec980896dab7727ae1621ca4e09cdfcf72a6a
Vitor Machado <vitorytre@gmail.com>
2025-06-04 06:56:06 -0700

    case class

---
+++ elegant-objects/src/main/scala/Bakery.scala
@@ -0,0 +1,19 @@
+private trait Bakery {
+  def cookBrownie(): Food
+  def brewCupOfCoffee(flavor: String): Drink
+}
+
+object Bakery extends Bakery {
+  override def cookBrownie(): Food = {
+    Food("Brownie")
+  }
+
+  override def brewCupOfCoffee(flavor: String): Drink = {
+    new Drink(flavor)
+  }
+}
+
+case class Food(name: String)
+class Drink(val flavor: String)
+
+// case class have a built-in apply method, so we can create instances without using the `new` keyword
