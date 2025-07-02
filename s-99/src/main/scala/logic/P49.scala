32ddf4a99b72b9319d4cde0dead7a00ec929dee0
Vitor Machado <vitorytre@gmail.com>
2025-07-02 05:07:31 -0700

    s99

--- s-99/src/main/scala/logic/P49.scala
+++ s-99/src/main/scala/logic/P49.scala
@@ -5,14 +5,14 @@ object P49 {
   def gray(n: Int): List[String] =
     if (n == 0) List("")
     else { val lower = gray(n - 1)
-      (lower map { "0" + _ }) ::: (lower.reverse map { "1" + _ })
+      lower.map { "0" + _ } ::: lower.reverse.map { "1" + _ }
     }
 
   private val strings = mutable.Map(0 -> List(""))
   def grayMemoized(n: Int): List[String] = {
     if (!strings.contains(n)) {
-      strings(n) = (grayMemoized(n - 1) map { "0" + _ }) :::
-                   (grayMemoized(n - 1).reverse map { "1" + _ })
+      strings(n) = grayMemoized(n - 1).map { "0" + _ } :::
+                   grayMemoized(n - 1).reverse.map { "1" + _ }
     }
     strings(n)
   }
