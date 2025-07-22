4d85ab785b2f78c8e304a3ff15045613ca45aca3
Vitor Machado <vitorytre@gmail.com>
2025-07-22 19:25:05 -0300

    lists

--- functional-collections/src/main/scala/lists.scala
+++ functional-collections/src/main/scala/lists.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 val fruits = List("Apple", "Orange", "Banana")
 val nums = 1 :: 2 :: Nil
 // same as 1 :: (2 :: Nul)
@@ -13,4 +15,17 @@ def lists(): Unit = {
   nums match {
     case x :: y :: _ => println(x + y) // 3
   }
+
+  println(s"last: ${(fruits.last)}")
+  println(s"head: ${(fruits.head)}")
+}
+
+//complexity of head is constant time.
+// last takes steps proportional to the length of the list xs.
+// init is analogous to last
+@tailrec
+def last[T](xs: List[T]): T = xs match {
+  case List() => throw Error("last of empty list")
+  case List(x) => x
+  case y :: ys => last(ys)
 }
