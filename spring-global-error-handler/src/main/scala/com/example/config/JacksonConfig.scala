393d7871f9451674702676fef45b9ab0f13adeb5
Vitor Machado <vitorytre@gmail.com>
2025-10-27 10:28:36 -0300

    spring global error handler

--- spring-global-error-handler/src/main/scala/com/example/config/JacksonConfig.scala
+++ spring-global-error-handler/src/main/scala/com/example/config/JacksonConfig.scala
@@ -6,5 +6,26 @@ import com.fasterxml.jackson.databind.Module
 
 @Configuration
 class JacksonConfig:
-  @Bean
+  @Bean // SB auto-configuration automatically detects any Module beans and register them with the ObjectMapper
   def scalaModule(): Module = DefaultScalaModule
+  // the scalaModule method configures Jackson to work with Scala types by 
+  // registering the DefaultScalaModule.
+
+
+// DefaultScalaModule: This jackson module adds serialization and deserialization support for Scala-specific features:
+  // - Option types (converts Some(value) to value, None to null)
+  // - Case classes: maps json fields to constructor parameters - Handles the apply and copy methods.
+  
+// Integration flow:
+  
+//  Spring Boot startup
+//  ↓
+//  Scans for @Configuration classes
+//  ↓
+//  Finds JacksonConfig
+//  ↓
+//  Executes @Bean methods
+//  ↓
+//  Registers DefaultScalaModule with ObjectMapper
+//  ↓
+//  ObjectMapper now understands Scala types
