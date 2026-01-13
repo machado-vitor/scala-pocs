3c68c953c9be863105cef30282dd85be2a899dfb
Vitor Machado <vitorytre@gmail.com>
2026-01-13 13:19:30 -0300

    P55: binary tree

--- s-99/src/main/scala/binarytree/tree1.scala
+++ s-99/src/main/scala/binarytree/tree1.scala
@@ -16,8 +16,33 @@ package binarytree {
     def apply[T](value: T): Node[T] = Node(value, End, End)
   }
 
+  // P55
+  object Tree {
+    def cBalanced[T](n: Int, value: T): List[Tree[T]] =
+      n match {
+        case 0 => List(End)
+        case _ =>
+          val subtreeNodes = n - 1 // 1 node is the root, so we remove it, the rest go to children
+          val smallerHalf = subtreeNodes / 2
+          val largerHalf = subtreeNodes - smallerHalf
+          if (smallerHalf == largerHalf)
+            for {
+              left <- cBalanced(smallerHalf, value)
+              right <- cBalanced(smallerHalf, value)
+            } yield Node(value, left, right)
+          else
+            for {
+              left <- cBalanced(smallerHalf, value)
+              right <- cBalanced(largerHalf, value)
+              (l, r) <- List((left, right), (right, left))
+            } yield Node(value, l, r)
+      }
+  }
+
   object Tree1 extends App {
     val tree = Node("a", Node("b", Node("d"), Node("e")), Node("c", End, Node("f", Node("g"), End)))
     println(tree)
+
+    Tree.cBalanced(4, "x").foreach(println)
   }
 }
