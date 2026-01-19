fac0733c2d659ab3fd7c5ce6a08d5eaab57809f0
Vitor Machado <vitorytre@gmail.com>
2026-01-19 10:07:59 -0300

    refactoring

--- s-99/src/main/scala/binarytree/tree1.scala
+++ s-99/src/main/scala/binarytree/tree1.scala
@@ -5,6 +5,13 @@ package binarytree {
   // trait: supports multiple inheritance - intended for behavior and capabilities, weaker binary compatibility with java
   // and are better for mixins, and are good practice not hold state, it can cause conflict if other trait is used with same fields.
   // abstract class: only single inheritance - better for holding state
+
+  //  A - Invariant
+  // Can't have subtyping relationship, even though the contained types might be related by inheritance.
+
+  // +A - Covariant
+  // -A - Contravariant
+
   sealed abstract class Tree[+T] {
     def isMirrorOf[V](tree: Tree[V]): Boolean
     def isSymmetric: Boolean
