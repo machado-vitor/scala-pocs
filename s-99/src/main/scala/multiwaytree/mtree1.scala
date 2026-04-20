7809ff1986afb427076f7ad11aef1c3e039bc317
Vitor Machado <vitorytre@gmail.com>
2026-04-20 10:04:19 -0300

    multiway trees

--- s-99/src/main/scala/multiwaytree/mtree1.scala
+++ s-99/src/main/scala/multiwaytree/mtree1.scala
@@ -1,18 +1,40 @@
 package multiwaytree {
 
+  import scala.language.implicitConversions
+
   case class MTree[+T](value: T, children: List[MTree[T]]) {
     def this(value: T) = this(value, List())
-    override def toString: String = "M(" + value.toString + " {" + children.map(_.toString).mkString(",") + "})"
+    override def toString: String = value.toString + children.map(_.toString).mkString + "^"
   }
 
   object MTree {
     def apply[T](value: T) = new MTree(value, List())
     def apply[T](value: T, children: List[MTree[T]]) = new MTree(value, children)
+
+    // P70: parse a node string back into a multiway tree.
+    implicit def string2MTree(s: String): MTree[Char] = {
+      def parse(pos: Int): (MTree[Char], Int) = {
+        val value = s(pos)
+        var i = pos + 1
+        val children = List.newBuilder[MTree[Char]]
+        while (s(i) != '^') {
+          val (child, next) = parse(i)
+          children += child
+          i = next
+        }
+        (MTree(value, children.result()), i + 1) // skip the '^'
+      }
+      parse(0)._1
+    }
   }
 
   object MTree1 extends App {
     val tree = MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e')))))
-    println(tree)
-    // M(a {M(f {M(g {})}),M(c {}),M(b {M(d {}),M(e {})})})
+
+    // P70
+    println(tree.toString)
+    // afg^^c^bd^e^^^
+    println(MTree.string2MTree("afg^^c^bd^e^^^").toString)
+    // afg^^c^bd^e^^^
   }
 }
