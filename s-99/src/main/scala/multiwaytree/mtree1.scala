4e42458bb8a2218a24c20b391ac6da885b497598
Vitor Machado <vitorytre@gmail.com>
2026-04-22 09:40:22 -0300

    multiway trees p71

--- s-99/src/main/scala/multiwaytree/mtree1.scala
+++ s-99/src/main/scala/multiwaytree/mtree1.scala
@@ -1,13 +1,18 @@
 package multiwaytree {
 
-  import scala.language.implicitConversions
-
   case class MTree[+T](value: T, children: List[MTree[T]]) {
     def this(value: T) = this(value, List())
     override def toString: String = value.toString + children.map(_.toString).mkString + "^"
 
     // P70C: count the nodes of a multiway tree.
     def nodeCount: Int = 1 + children.map(_.nodeCount).sum
+
+    // P71: sum of path lengths from root to all nodes.
+    def internalPathLength: Int = {
+      def ipl(tree: MTree[T], depth: Int): Int =
+        depth + tree.children.map(ipl(_, depth + 1)).sum
+      ipl(this, 0)
+    }
   }
 
   object MTree {
@@ -15,7 +20,7 @@ package multiwaytree {
     def apply[T](value: T, children: List[MTree[T]]) = new MTree(value, children)
 
     // P70: parse a node string back into a multiway tree.
-    implicit def string2MTree(s: String): MTree[Char] = {
+    def fromString(s: String): MTree[Char] = {
       def parse(pos: Int): (MTree[Char], Int) = {
         val value = s(pos)
         var i = pos + 1
@@ -31,13 +36,16 @@ package multiwaytree {
     }
   }
 
+  extension (s: String)
+    def toMTree: MTree[Char] = MTree.fromString(s)
+
   object MTree1 extends App {
     val tree = MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e')))))
 
     // P70
     println(tree.toString)
     // afg^^c^bd^e^^^
-    println(MTree.string2MTree("afg^^c^bd^e^^^").toString)
+    println("afg^^c^bd^e^^^".toMTree.toString)
     // afg^^c^bd^e^^^
 
     // P70C
@@ -45,5 +53,9 @@ package multiwaytree {
     // 2
     println(tree.nodeCount)
     // 7
+
+    // P71
+    println("afg^^c^bd^e^^^".toMTree.internalPathLength)
+    // 9
   }
 }
